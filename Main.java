9a4c9fdc3c32c26b5a1bdd9240e765473f31a704
sdrobyshevsky <sdrobyshevsky@mail.ru>
2023-10-07 23:50:55 +0300

    Added info about task

--- Main.java
+++ Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) throws CloneNotSupportedException {
-        PhoneBook addressBook = new PhoneBook();
-        addressBook.addSave("123-11-32", "Иван Иванов", "ул. Ленина, 3");
-        addressBook.addSave("883-47-21", "Петр Петров", "пр. Мира");
-        addressBook.addSave("919-41-85", "Николай Николаев", "пер. Славы");
-        System.out.println(addressBook.allSaves());
+        PhoneBook PhoneBook = new PhoneBook();
+        PhoneBook.addSave("123-11-32", "Иван Иванов", "ул. Ленина, 3");
+        PhoneBook.addSave("883-47-21", "Петр Петров", "пр. Мира");
+        PhoneBook.addSave("919-41-85", "Николай Николаев", "пер. Славы");
+        System.out.println(PhoneBook.allSaves());
     }
 }
